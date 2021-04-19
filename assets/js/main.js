c82d6247a314d13187c60dc79cab498d6a4d49c2
AriChou <46300506+Ari-Chou@users.noreply.github.com>
2021-04-19 19:58:09 +0800

    resume

---
+++ assets/js/main.js
@@ -0,0 +1,125 @@
+/* SHOW MENU */
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  nav = document.getElementById(navId);
+
+  if (toggle && navId) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show-menu');
+    });
+  }
+};
+showMenu('nav-toggle', 'nav-menu');
+
+/* REMOVE MENU MOBILE */
+const navLink = document.querySelectorAll('.nav__link');
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach((l) => l.addEventListener('click', linkAction));
+
+/* SCROLL SECTIONS ACTIVE LINK */
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active-link');
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
+
+/* SHOW SCROLL TOP */
+function scrollTop() {
+  const scrollTop = document.getElementById('scroll-top');
+  const scrollY = window.pageYOffset;
+  if (scrollY >= 200) {
+    scrollTop.classList.add('show-scroll');
+  } else {
+    scrollTop.classList.remove('show-scroll');
+  }
+}
+window.addEventListener('scroll', scrollTop);
+
+/* DARK LIGHT THEME */
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bxs-sun';
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'bxs-moon' : 'bxs-sun';
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === 'bxs-moon' ? 'add' : 'remove'](
+    iconTheme
+  );
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+// /* Scale the cv when usee want donwload the CV */
+// function scaleCv() {
+//   document.body.classList.add('scale-cv');
+// }
+
+// //when user downloaded the cv remove the scall class
+// function removeScale() {
+//   document.body.classList.remove('scale-cv');
+// }
+
+// /* Generate PDF */
+// let resumeButton = document.getElementById('resume-button');
+// let areaCv = document.getElementById('area-cv');
+
+// let pdfFormate = {
+//   margin: 0,
+//   filename: '周义明(前端开发).pdf',
+//   html2canvas: { scale: 4 },
+//   jsPDF: { format: 'a4' },
+// };
+
+// function generateResume() {
+//   html2pdf(areaCv, pdfFormate);
+// }
+
+// resumeButton.addEventListener('click', () => {
+//   scaleCv();
+//   generateResume();
+
+//   setTimeout(removeScale, 5000);
+// });
